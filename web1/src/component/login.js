67eb19cf27d80127d7502376d453342116f688c4
Arun Chandra <arunchandra19082004@gmail.com>
2023-07-18 16:09:23 +0530

    Update login.js

--- web1/src/component/login.js
+++ web1/src/component/login.js
@@ -10,6 +10,9 @@ function Login() {
           <div className="form-group">
             <input type="email" id="email" name="email" placeholder="Email" required />
           </div>
+<div className="form-group">
+            <input type="email" id="email" name="email" placeholder="Email" required />
+          </div>
           <div className="form-group">
             <input type="password" id="password" name="password" placeholder="Password" required />
           </div>
