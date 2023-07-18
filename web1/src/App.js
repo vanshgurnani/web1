6c216a54d466042262c3d042ce0558953954ef9a
Vansh Gurnani <gurnanivansh57@gmail.com>
2023-07-18 15:15:57 +0530

    login done

--- web1/src/App.js
+++ web1/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from '../src/component/login';
+import Register from '../src/component/register';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path='' element={<Login />}/>
+        <Route exact path='register' element={<Register />}/>
       </Routes>
     </Router>
   );
