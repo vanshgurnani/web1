6352850ed580dac76630fdc939c257c9219d9c7c
Vansh Gurnani <gurnanivansh57@gmail.com>
2023-07-19 17:44:38 +0530

    done

--- web1/frontend/src/App.js
+++ web1/frontend/src/App.js
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from '../src/component/login';
 import Register from '../src/component/register';
-import Detail from '../src/component/ProductDetail';
-import Product from '../src/component/product';
 
 function App() {
   return (
@@ -10,8 +8,6 @@ function App() {
       <Routes>
         <Route exact path='' element={<Login />}/>
         <Route exact path='/register' element={<Register />}/>
-        <Route exact path='/Detail' element={<Detail />}/>
-        <Route exact path='/product' element={<Product />}/>
       </Routes>
     </Router>
   );
