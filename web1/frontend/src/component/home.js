18c1840b38541cf61f700c7452624354d045b8d3
Vansh Gurnani <gurnanivansh57@gmail.com>
2023-07-20 22:53:12 +0530

    done

--- web1/frontend/src/component/home.js
+++ web1/frontend/src/component/home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../component/home.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Navbar from './navbar';
 import Carousel from './carousel';
 import Footer from './footer';
